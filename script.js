c82af6b472e393963cf124b14d2939bda27f1fba
Abineshkumar07 <129647868+Abineshkumar07@users.noreply.github.com>
2023-04-06 18:09:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+      if (firstLetter.toLocaleLowerCase() == "j") {
+        console.log('Good Bye ' + names[i]);
+        byeSpeaker.speak(names[i]);
+      } else {
+        console.log('Hello ' + names[i]);
+        helloSpeaker.speak(names[i]);
+      }
+    
